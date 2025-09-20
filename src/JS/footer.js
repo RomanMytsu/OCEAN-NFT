8fee3c15cc1fbcc8f9c1b4ae31519d01f4e2b485
Roman <romanmytsu@gmail.com>
2025-09-20 12:44:49 +0300

    add footer

--- src/JS/footer.js
+++ src/JS/footer.js
@@ -1,14 +1,14 @@
 
 const showItems = document.querySelectorAll(".footer-menu-item");
 
-if (window.innerWidth < 1439) {
+if (window.innerWidth < 1438) {
   showItems.forEach((item) => {
     const title = item.querySelector(".footer-menu-item-title");
     const marker = item.querySelector(".footer-list-icon");
     const content = item.querySelector(".submenu");
 
     title.addEventListener("click", () => {
-      show.forEach((i) => {
+      showItems.forEach((i) => {
         if (i !== item) {
           i.querySelector(".submenu").classList.remove("active");
           i.querySelector(".footer-list-icon").classList.remove("active");
